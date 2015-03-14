2ca2490010442efd283832f76cf307e6577792e4
Sergi Granell <xerpi.g.12@gmail.com>
2015-03-14 20:48:07 +0100

    First commit

---
+++ libsf2d/include/sf2d.h
@@ -0,0 +1,8 @@
+#ifndef SF2D_H
+#define SF2D_H
+
+
+int sf2d_init();
+int sf2d_fini();
+
+#endif
---
+++ libsf2d/source/sf2d.c
@@ -0,0 +1,13 @@
+#include "sf2d.h"
+
+int sf2d_init()
+{
+
+	
+	return 0;
+}
+
+int sf2d_fini()
+{
+	return 0;
+}
