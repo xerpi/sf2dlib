506b741bd5366190e953e63dfee084ab8be1cb0b
Sergi Granell <xerpi.g.12@gmail.com>
2015-05-06 21:52:08 +0200

    Added touch to the sample :P

--- sample/source/main.c
+++ sample/source/main.c
@@ -40,11 +40,19 @@ int main()
 	sf2d_texture *tex2 = sf2d_create_texture_mem_RGBA8(citra_img.pixel_data, citra_img.width, citra_img.height, GPU_RGBA8, SF2D_PLACE_RAM);
 
 	float rad = 0.0f;
+	u16 touch_x = 320/2;
+	u16 touch_y = 240/2;
+	touchPosition touch;
 
 	while (aptMainLoop()) {
 
 		hidScanInput();
 		if (hidKeysDown() & KEY_START) break;
+		if (hidKeysHeld() & KEY_TOUCH) {
+			hidTouchRead(&touch);
+			touch_x = touch.px;
+			touch_y = touch.py;
+		}
 
 		sf2d_start_frame(GFX_TOP, GFX_LEFT);
 
@@ -61,7 +69,7 @@ int main()
 			draw_triangle();
 			sf2d_draw_rectangle_rotate(190, 160, 70, 60, RGBA8(0xFF, 0xFF, 0xFF, 0xFF), 3.0f*rad);
 			sf2d_draw_rectangle(30, 100, 40, 60, RGBA8(0xFF, 0x00, 0xFF, 0xFF));
-			sf2d_draw_texture_rotate(tex2, 190, 120-tex2->height/2, -rad);
+			sf2d_draw_texture_rotate(tex2, touch_x-tex1->width/2, touch_y-tex1->height/2, -rad);
 			sf2d_draw_rectangle(160-15 + cosf(rad)*50.0f, 120-15 + sinf(rad)*50.0f, 30, 30, RGBA8(0x00, 0xFF, 0xFF, 0xFF));
 
 		sf2d_end_frame();
