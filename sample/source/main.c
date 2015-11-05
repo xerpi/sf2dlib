037533d253137970fba1099c3bad05baebca5ad6
Sergi Granell <xerpi.g.12@gmail.com>
2015-11-06 00:12:36 +0100

    Add stereoscopic 3D to the sample :D

--- sample/source/main.c
+++ sample/source/main.c
@@ -20,6 +20,8 @@ extern const struct {
   unsigned char	 pixel_data[];
 } dice_img;
 
+#define CONFIG_3D_SLIDERSTATE (*(float *)0x1FF81080)
+
 int main()
 {
 	// Set the random seed based on the time
@@ -27,11 +29,13 @@ int main()
 
 	sf2d_init();
 	sf2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
+	sf2d_set_3D(1);
 
 
 	sf2d_texture *tex1 = sf2d_create_texture_mem_RGBA8(dice_img.pixel_data, dice_img.width, dice_img.height, TEXFMT_RGBA8, SF2D_PLACE_RAM);
 	sf2d_texture *tex2 = sf2d_create_texture_mem_RGBA8(citra_img.pixel_data, citra_img.width, citra_img.height, TEXFMT_RGBA8, SF2D_PLACE_RAM);
 
+	float offset3d = 0.0f;
 	float rad = 0.0f;
 	u16 touch_x = 320/2;
 	u16 touch_y = 240/2;
@@ -55,7 +59,23 @@ int main()
 			sf2d_set_clear_color(RGBA8(rand()%255, rand()%255, rand()%255, 255));
 		}
 
+		offset3d = CONFIG_3D_SLIDERSTATE * 30.0f;
+
 		sf2d_start_frame(GFX_TOP, GFX_LEFT);
+			sf2d_draw_fill_circle(offset3d + 60, 100, 35, RGBA8(0x00, 0xFF, 0x00, 0xFF));
+			sf2d_draw_fill_circle(offset3d + 180, 120, 55, RGBA8(0xFF, 0xFF, 0x00, 0xFF));
+
+			sf2d_draw_rectangle_rotate(offset3d + 260, 20, 40, 40, RGBA8(0xFF, 0xFF, 0x00, 0xFF), -2.0f*rad);
+			sf2d_draw_rectangle(offset3d + 20, 60, 40, 40, RGBA8(0xFF, 0x00, 0x00, 0xFF));
+			sf2d_draw_rectangle(offset3d + 5, 5, 30, 30, RGBA8(0x00, 0xFF, 0xFF, 0xFF));
+			sf2d_draw_texture_rotate(tex1, offset3d + 400/2 + circle.dx, 240/2 - circle.dy, rad);
+		sf2d_end_frame();
+
+		sf2d_start_frame(GFX_TOP, GFX_RIGHT);
+
+			sf2d_draw_fill_circle(60, 100, 35, RGBA8(0x00, 0xFF, 0x00, 0xFF));
+			sf2d_draw_fill_circle(180, 120, 55, RGBA8(0xFF, 0xFF, 0x00, 0xFF));
+
 			sf2d_draw_rectangle_rotate(260, 20, 40, 40, RGBA8(0xFF, 0xFF, 0x00, 0xFF), -2.0f*rad);
 			sf2d_draw_rectangle(20, 60, 40, 40, RGBA8(0xFF, 0x00, 0x00, 0xFF));
 			sf2d_draw_rectangle(5, 5, 30, 30, RGBA8(0x00, 0xFF, 0xFF, 0xFF));
